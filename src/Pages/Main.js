68c4ee9a91a4020f036a78590a2d4b691b9b496a
ljb2727 <ljb2727@gmail.com>
2022-03-22 18:10:17 +0900

    save

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -15,6 +15,14 @@ function Main() {
     message: "",
   });
 
+  const [data, setData] = React.useState({
+    골프장: "",
+    전반: "",
+    후반: "",
+    날짜: "",
+    시간: "",
+  });
+
   function createScore() {
     const golfzone = document.getElementById("golfzone").value;
     const course1 = document.getElementById("course1").value;
@@ -27,13 +35,22 @@ function Main() {
         show: true,
         message: "먼저 골프장을 선택해주세요.",
       });
+      return null;
     }
     if (!course1) {
       setToastShow({
         show: true,
         message: "먼저 전반코스를 선택해주세요.",
       });
+      return null;
     }
+    setData({
+      골프장: golfzone,
+      전반: course1,
+      후반: course2,
+      날짜: date,
+      시간: time,
+    });
   }
   return (
     <>
@@ -71,6 +88,8 @@ function Main() {
             >
               생성
             </Button>
+
+            {`${data.골프장} / ${data.전반} / ${data.후반} / ${data.날짜} / ${data.시간}`}
           </Grid>
         </Grid>
       </Box>
