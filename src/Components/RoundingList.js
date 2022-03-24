11871e6922382241715fe01a50aadc0882d1a51a
ljb2727 <ljb2727@gmail.com>
2022-03-24 14:56:57 +0900

    customTheme 설정

---
+++ src/Components/RoundingList.js
@@ -0,0 +1,62 @@
+import React from "react";
+import useStore from "../Data/useStore";
+
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
+const RoundingList = () => {
+  const { info } = useStore();
+  // const info = [
+  //   {
+  //     id: 0,
+  //     골프장: "강남300",
+  //     전반: "a1",
+  //     후반: "a2",
+  //     날짜: "2022년 03월 24일",
+  //     시간: "오전 10시 30분",
+  //   },
+  // ];
+  return (
+    <>
+      {info.map((item) => {
+        const { id, 골프장, 전반, 후반, 날짜, 시간 } = item;
+
+        return (
+          <Item
+            key={id}
+            골프장={골프장}
+            전반={전반}
+            후반={후반}
+            날짜={날짜}
+            시간={시간}
+          ></Item>
+        );
+      })}
+    </>
+  );
+};
+
+const StyleItem = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  background: "#f9f7f9",
+  padding: theme.spacing(1),
+  marginBottom: theme.spacing(1),
+  color: theme.palette.text,
+}));
+
+function Item({ id, 골프장, 전반, 후반, 날짜, 시간 }) {
+  return (
+    <>
+      <StyleItem>
+        <Typography variant="body1">{날짜}</Typography>
+        <Typography variant="body1">{골프장}</Typography>
+        <Typography variant="body1">{`${전반}`}</Typography>
+      </StyleItem>
+    </>
+  );
+}
+
+export default RoundingList;
